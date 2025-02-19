658be87a50bcd6abf8bf9d4dbc8b1996e5f61a34
shtigersh <shtiger.sh@gmail.com>
2025-02-19 11:24:19 -0800

    run file

---
+++ run.py
@@ -0,0 +1,10 @@
+import subprocess
+
+# تشغيل file1.py
+subprocess.Popen(['python', 'main.py'])
+
+# تشغيل file2.py
+subprocess.Popen(['python', 'HUB.py'])
+
+# تشغيل file3.py
+subprocess.Popen(['python', 'emploe.py'])
