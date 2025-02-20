38d69e766b734036cb686fcd9819a3fa3bd24de3
shtigersh <shtiger.sh@gmail.com>
2025-02-20 10:13:24 -0800

    my changes2

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system('start python main.py & start python emploe.py & start python HUB.py')
-
-
-
-
-
-
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python bot1.py &
+python bot2.py &
+python bot3.py &
+wait
